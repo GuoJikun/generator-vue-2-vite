f3bd3be35959af6f3e9d4560fed92d1eab52756b
GuoJikun <guojikun486546@163.com>
2023-09-05 14:53:09 +0800

    ✨ feat: 更新

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,6 +4,8 @@ const chalk = require("chalk");
 const yosay = require("yosay");
 
 module.exports = class extends Generator {
+  initializing() {}
+
   prompting() {
     // Have Yeoman greet the user.
     this.log(
@@ -14,27 +16,59 @@ module.exports = class extends Generator {
 
     const prompts = [
       {
+        type: "text",
+        name: "title",
+        message: "请输入项目名称"
+      },
+      {
         type: "confirm",
-        name: "someAnswer",
-        message: "Would you like to enable this option?",
-        default: true
+        name: "router",
+        message: "Can you confirm?",
+        initial: true
+      },
+      {
+        type: "checkbox",
+        name: "components",
+        message: "Select UI Components",
+        choices: [
+          { title: "ElementUI", value: "element-ui" },
+          { title: "ElementUI", value: "element-ui1" },
+          { title: "ElementUI", value: "element-ui2" },
+          { title: "ElementUI", value: "element-ui3" },
+          { title: "ElementUI", value: "element-ui4" },
+          { title: "ElementUI", value: "element-ui5" },
+          { title: "ViewDesign(IView)", value: "view-design" }
+        ]
       }
     ];
 
     return this.prompt(prompts).then(props => {
       // To access props later use this.props.someAnswer;
+      console.log(props);
       this.props = props;
     });
   }
 
   writing() {
-    this.fs.copy(
-      this.templatePath("dummyfile.txt"),
-      this.destinationPath("dummyfile.txt")
-    );
+    this.fs.copy(this.templatePath(), this.destinationPath(this.props.title));
   }
 
   install() {
-    this.installDependencies();
+    this.spawnCommandSync("ls");
+    this.spawnCommandSync(`cd ./${this.props.title}`);
+    console.log(`cd ${this.props.title}`);
+    this.yarnInstall("element-ui");
+    // this.installDependencies({
+    //   yarn: true,
+    //   npm: false,
+    //   bower: false,
+    //   skipMessage: true
+    // });
+    // this.run("yarn", this.props.title);
+  }
+
+  end() {
+    this.spawnCommandSync(`cd ./${this.props.title}`);
+    this.spawnCommandSync("pwd");
   }
 };
